94e1d5bff166153f2dc034dd7cf4c3425468e214
SilverGold <mcredstonewing@gmail.com>
2023-05-29 02:29:27 -0400

    Added Danishes

--- src/main/java/net/silvergold/gourmet/fluid/ModFluidTypes.java
+++ src/main/java/net/silvergold/gourmet/fluid/ModFluidTypes.java
@@ -15,24 +15,22 @@ public class ModFluidTypes {
     public static final ResourceLocation SEED_OIL_FLOWING_RL = new ResourceLocation("block/water_flow");
     public static final ResourceLocation SEED_OIL_RL = new ResourceLocation(Gourmet.MOD_ID, "misc/seed_oil_overlay");
     //Batter
-    public static final ResourceLocation Batter_STILL_RL = new ResourceLocation("block/water_still");
-    public static final ResourceLocation Batter_FLOWING_RL = new ResourceLocation("block/water_flow");
-    public static final ResourceLocation Batter_RL = new ResourceLocation(Gourmet.MOD_ID, "misc/seed_oil_overlay");
+    public static final ResourceLocation BATTER_STILL_RL = new ResourceLocation(Gourmet.MOD_ID, "misc/batter_still");
+    public static final ResourceLocation BATTER_FLOWING_RL = new ResourceLocation(Gourmet.MOD_ID, "misc/batter_flow");
+    public static final ResourceLocation BATTER_RL = new ResourceLocation(Gourmet.MOD_ID, "misc/seed_oil_overlay");
 
     public static final DeferredRegister<FluidType> FLUID_TYPES =
             DeferredRegister.create(ForgeRegistries.Keys.FLUID_TYPES, Gourmet.MOD_ID);
 
     //Seed Oil
-    public static final RegistryObject<FluidType> SEED_OIL_FLUID_TYPE = register("seed_oil",
-            FluidType.Properties.create().lightLevel(2).density(15).viscosity(5));
+    public static final RegistryObject<FluidType> SEED_OIL_FLUID_TYPE = FLUID_TYPES.register("seed_oil", () -> new BaseFluidType(SEED_OIL_STILL_RL, SEED_OIL_FLOWING_RL, SEED_OIL_RL,
+            0xA1DCB506, new Vector3f(224f / 255f, 56f / 255f, 208f / 255f), FluidType.Properties.create().lightLevel(2).density(15).viscosity(5)));
     //Batter
-    public static final RegistryObject<FluidType> BATTER_FLUID_TYPE = register("batter",
-            FluidType.Properties.create().lightLevel(2).density(15).viscosity(5));
+    public static final RegistryObject<FluidType> BATTER_FLUID_TYPE = FLUID_TYPES.register("batter", () -> new BaseFluidType(BATTER_STILL_RL, BATTER_FLOWING_RL, SEED_OIL_RL,
+            0xFFFFFFFF, new Vector3f(1.0f, 1.0f, 1.0f), FluidType.Properties.create().lightLevel(2).density(15).viscosity(5)));
 
-    private static RegistryObject<FluidType> register(String name, FluidType.Properties properties) {
-        return FLUID_TYPES.register(name, () -> new BaseFluidType(SEED_OIL_STILL_RL, SEED_OIL_FLOWING_RL, SEED_OIL_RL,
-                0xA1DCB506, new Vector3f(224f / 255f, 56f / 255f, 208f / 255f), properties));
-    }
+
+
 
     public static void register(IEventBus eventBus) {
         FLUID_TYPES.register(eventBus);
